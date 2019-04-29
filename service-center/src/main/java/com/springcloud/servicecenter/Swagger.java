775b8ee54405cf888c5122964798cac903702b21
Tu Zhanle <tzhanl@hotmail.com>
2019-04-29 03:20:29 -0700

    add swagger

---
+++ service-center/src/main/java/com/springcloud/servicecenter/Swagger.java
@@ -0,0 +1,23 @@
+package com.springcloud.servicecenter;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage())
+    }
+
+    private ApiInfo apiInfo() {
+    }
+}
